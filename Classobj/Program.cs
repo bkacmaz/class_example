641e92fd8566b3282d833dec2e5aaf9d0866fa83
bkacmaz <berfinnkacmaz@gmail.com>
2022-01-22 16:44:53 +0300

    first commit

---
+++ Classobj/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Classobj
+{
+
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            hesap hsp = new hesap(1,2);
+            object tpl=hsp.toplam();
+
+            hsp.verial();
+            object tpl3 = hsp.toplam();
+
+            hesap hsp2 = new hesap(1.2, 3.4);
+            object tpl2 = hsp2.toplam();
+
+            double deger = (double)tpl2;
+            double cikar = hsp2.dcıkar();
+           
+        }
+    }
+}
---
+++ Classobj/hesap.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classobj
+{
+    class hesap
+    {
+        public int inum1;
+        int inum2;
+        public double dnum1;
+        double dnum2;
+        //constructor
+        bool tip;
+        
+        public hesap(int n1, int n2)
+        {
+            inum1 = n1;
+            inum2 = n2;
+            tip = true;
+        }
+        public hesap(double n1, double n2)
+        {
+            dnum1 = n1;
+            dnum2 = n2;
+            tip = false;
+        }
+        public object toplam()
+        {
+            if (tip)
+            {
+                return inum1 + inum2;
+            }
+            else
+            {
+                return dnum1 + dnum2;
+            }
+            
+        }
+
+        public int icıkar()
+        {
+            return inum1 - inum2;
+        }
+        public double dcıkar()
+        {
+            return dnum1 - dnum2;
+        }
+
+        public void verial()
+        {
+            Console.Write("deger1: ");
+            inum1=int.Parse(Console.ReadLine());
+            Console.Write("deger2: ");
+            inum2 = int.Parse(Console.ReadLine());
+        }
+
+    }
+}
